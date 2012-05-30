3690214785048168141ecdac3ca20bb24d2baacb
Ilya Vorontsov <prijutme4ty@gmail.com>
2012-05-30 20:15:44 +0400

    same? --> same_by; test/unit changed to rspec in ptap, pmap, same_by

---
+++ spec/same_by_spec.rb
@@ -0,0 +1,54 @@
+require 'spec_helper'
+require 'bioinform/support/same_by'
+
+describe Enumerable do
+  descibe 'same_by?' do
+    it 'should be work with both hashes and arrays' do
+      ['a','b','c'].same_by?{|k| k.length}
+      {'a'=>13,'b'=>12,'c'=>14}.same_by?{|k,v| v < 20}
+    end
+    it 'should be true for empty collections' do
+      [].same_by?(&:length).should be_true
+      [].same_by?.should be_true
+    end
+    context 'without block' do
+      it 'should compare if all elements of collection are the same' do
+        %w{cat cat cat}.same_by?.should be_true
+        %w{cat dog rat}.same_by?.should be_false
+      end
+    end
+    context 'with a block' do
+      it 'should compare enumerables by a value of block' do
+        %w{cat dog rat}.same_by?(&:length).should be_true
+        %w{cat dog rabbit}.same_by?(&:length).should be_false
+      end
+      it 'should be true if all elements are true' do
+        [4,8,2,2].same_by?(&:even?).should be_true
+      end
+      it 'should be true if all elements are false' do
+        [1,3,9,7].same_by?(&:even?).should be_true
+      end
+      it 'should be false if some elements are true and some are false' do
+        [1,8,3,2].same_by?(&:even?).should be_false
+      end
+    end
+  end
+end
+
+
+class TestEnumerableSame < Test::Unit::TestCase
+  def test_same
+    assert_equal(true, [1,3,9,7].same_by?(&:even?))
+    assert_equal(true, [4,8,2,2].same_by?(&:even?))
+    assert_equal(false, [1,8,3,2].same_by?(&:even?))
+    
+    assert_equal(true, %w{cat dog rat}.same_by?(&:length))
+    assert_equal(false, %w{cat dog rabbit}.same_by?(&:length))
+    
+    assert_equal(true, %w{cat cat cat}.same_by?)
+    assert_equal(false, %w{cat dog rat}.same_by?)
+    
+    assert_equal(true, [].same_by?(&:length))
+    assert_equal(true, [].same_by?)
+  end
+end
