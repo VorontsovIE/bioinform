8359a96317c53c9163f7b942dc3b28078721b452
Ilya Vorontsov <prijutme4ty@gmail.com>
2012-07-04 12:59:47 +0400

    empty classes PCM, PPM are now connected

--- lib/bioinform/data_models.rb
+++ lib/bioinform/data_models.rb
@@ -1,6 +1,6 @@
 require 'bioinform/data_models/pm'
-#require 'bioinform/data_models/pcm'
+require 'bioinform/data_models/pcm'
 require 'bioinform/data_models/pwm'
-#require 'bioinform/data_models/ppm'
+require 'bioinform/data_models/ppm'
 #require 'bioinform/data_models/iupac_word'
 #require 'bioinform/data_models/iupac_wordset'
