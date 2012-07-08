ec66c03279b502959e1b2c369334a792a74c759a
Ilya Vorontsov <prijutme4ty@gmail.com>
2012-07-08 15:24:09 +0400

    Hash#partial_sums

--- lib/bioinform/support/partial_sums.rb
+++ lib/bioinform/support/partial_sums.rb
@@ -4,3 +4,11 @@ class Array
     map{|el| sums += el}
   end
 end
+
+class Hash
+# {1 => 5, 4 => 3, 3 => 2}.partial_sums == {1=>5, 3=>7, 4=>10}
+  def partial_sums(initial = 0.0)
+    sums = initial
+    sort.collect_hash{|k,v| [k, sums += v]}
+  end
+end
