ef5f93c5b250ed69c92734a8c8231e4e3683618b
Ilya Vorontsov <prijutme4ty@gmail.com>
2012-08-28 03:16:24 +0400

    small refactoring

--- lib/bioinform/parsers/parser.rb
+++ lib/bioinform/parsers/parser.rb
@@ -9,34 +9,37 @@ module Bioinform
     end
     
     def parse!
-      inp = input
-      transpose =  inp.is_a?(Hash)
-      inp = ClassMethods.try_convert_to_array(inp)
-      inp.map!{|x| ClassMethods.try_convert_to_array(x)}
-      transpose = true  if (not inp.all?{|x| x.size == 4}) && inp.size == 4 && inp.same_by?(&:size)
-      @matrix = transpose ? inp.transpose : inp
-      result
+      inp = self.class.try_convert_to_array(input).map{|x| self.class.try_convert_to_array(x)}
+      should_transpose = input.is_a?(Hash) || (inp.size == 4) && inp.same_by?(&:size) && !inp.all?{|x| x.size == 4}
+      @matrix = should_transpose ? inp.transpose : inp
+      parsing_result
     end
     
     def parse
       parse!  rescue {}
     end
       
-    def result(options={})
+    def parsing_result(options={})
       raise 'Parsing Error' unless matrix.is_a?(Array) && ! matrix.empty? && matrix.all?(&:is_a?.(Array)) && matrix.all?{|pos| pos.size == 4} && matrix.all?(&:all?.(&:is_a?.(Numeric)))
       options.merge(matrix: @matrix)
     end
+
+    def self.array_from_acgt_hash(hsh)
+      hsh = hsh.collect_hash{|key,value| [key.to_s.upcase, value] }
+      raise 'some of hash keys A,C,G,T are missing or hash has excess keys' unless hsh.keys.sort == %w[A C G T]
+      %w[A C G T].collect{|letter| hsh[letter] }
+    end
     
-    class ClassMethods
-      def self.array_from_acgt_hash(hsh)
-        hsh = hsh.collect_hash{|key,value| [key.to_s.upcase, value] }
-        raise 'some of hash keys A,C,G,T are missing or hash has excess keys' unless hsh.keys.sort == %w[A C G T]
-        %w[A C G T].collect{|letter| hsh[letter] }
-      end
-      def self.try_convert_to_array(inp)
-        return inp  if inp.is_a? Array
-        array_from_acgt_hash(inp)
+    def self.try_convert_to_array(input)
+      case input
+      when Array
+        input
+      when Hash
+        array_from_acgt_hash(input)
+      else 
+        raise TypeError, 'input of Bioinform::Parser::array_from_acgt_hash should be Array or Hash'
       end
     end
+    
   end
 end
