cd7e9fed5dc6ebfb61da81146fbb7197272b61b9
Ilya Vorontsov <prijutme4ty@gmail.com>
2012-09-05 03:04:39 +0400

    make FantomParser able to parse multilple motifs and motifs with additional rows

--- lib/bioinform/parsers/string_fantom_parser.rb
+++ lib/bioinform/parsers/string_fantom_parser.rb
@@ -4,11 +4,32 @@ require 'bioinform/parsers/string_parser'
 module Bioinform
   class StringFantomParser < StringParser
     def header_pat
-      /NA (?<name>[\w.+:-]+)\n[\w\d]+ A C G T\n/
+      /NA (?<name>[\w.+:-]+)\n[\w\d]+ A C G T.*\n/
     end
     
     def row_pat
       /[\w\d]+ (?<row>(#{number_pat} )*#{number_pat})\n?/
     end
+    
+    def scan_splitter
+      scanner.scan(/(\/\/\n)+/)
+    end
+
+    def parse_matrix
+      matrix = []
+      while row_string = scan_row
+        matrix << split_row(row_string)[0,4]
+      end
+      matrix.transpose
+    end
+    
+    def parse!
+      scan_any_spaces
+      scan_splitter
+      name = parse_name
+      matrix = parse_matrix
+      Parser.parse!(matrix).merge(name: name)
+    end
+
   end
 end
--- spec/parsers/string_fantom_parser_spec.rb
+++ spec/parsers/string_fantom_parser_spec.rb
@@ -3,6 +3,43 @@ require 'bioinform/parsers/string_fantom_parser'
 
 module Bioinform    
   describe StringFantomParser do
+    describe '#parse' do
+      it 'should be able to parse several motifs' do
+        input = <<-EOS
+//
+NA  motif_1
+P0	A	C	G	T	
+P1	0	1	2	3	
+P2	4	5	6	7
+//
+//
+NA  motif_2
+P0	A	C	G	T	
+P1  1 2 3 4
+P2  5 6 7 8
+P3  9 10 11 12
+//
+NA  motif_3
+P0	A	C	G	T	
+P1	2 3	4 5
+P2	6 7 8 9
+        EOS
+        StringFantomParser.split(input).should == [ {matrix: [[0,1,2,3],[4,5,6,7]], name: 'motif_1'}, 
+                                                    {matrix: [[1,2,3,4],[5,6,7,8],[9,10,11,12]], name: 'motif_2'},
+                                                    {matrix: [[2,3,4,5],[6,7,8,9]], name: 'motif_3'} ]
+      end
+      
+      it 'should be able to parse motif with additional rows' do
+        input = <<-EOS
+NA  motif_1
+P0	A C G T S P
+P1	0 1 2 3 5 10
+P2	4 5 6 7 5 11
+        EOS
+        StringFantomParser.split(input).should == [ {matrix: [[0,1,2,3],[4,5,6,7]], name: 'motif_1'} ]
+      end
+    end
+  
     good_cases = {
       'string in Fantom-format' => {input: "
         NA  motif_CTNCAG
