f4b04cccba42961f1c738685dda6499f80f29ef0
Ilya Vorontsov <prijutme4ty@gmail.com>
2012-09-15 01:28:09 +0400

    parameters

---
+++ lib/bioinform/support/parameters.rb
@@ -0,0 +1,19 @@
+require 'ostruct'
+module Parameters
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
+  module ClassMethods
+    def make_parameters(*params)
+      params.each do |param|
+        define_method(param){ parameters.send(param) }
+        define_method("#{param}="){|new_value| parameters.send("#{param}=", new_value) }
+      end
+    end
+  end
+  def parameters; @parameters ||= OpenStruct.new; end
+  def set_parameters(hsh)
+    hsh.each{|k,v| send("#{k}=", v) }
+    self
+  end
+end
