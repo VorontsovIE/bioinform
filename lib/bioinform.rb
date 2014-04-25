60568d2ae2f948ea6e54123f56ce7ff554356c17
Ilya Vorontsov <prijutme4ty@gmail.com>
2014-04-26 03:29:16 +0400

    add class for lib-specific errors; get_pcm/get_ppm/get_pwm from different data models

--- lib/bioinform.rb
+++ lib/bioinform.rb
@@ -6,5 +6,32 @@ require_relative 'bioinform/data_models'
 require_relative 'bioinform/cli'
 
 module Bioinform
-  # Your code goes here...
+  class Error < StandardError
+  end
+
+  def self.get_pwm(data_model, matrix, background, pseudocount, effective_count)
+    pm = Bioinform.const_get(data_model).new(matrix)
+    pm.set_parameters(background: background)
+    if pseudocount && ! pseudocount.blank? && [:PCM,:PPM].include?(data_model.to_sym)
+      pm.set_parameters(pseudocount: pseudocount)
+    end
+    if effective_count && [:PPM].include?(data_model.to_sym)
+      pm.set_parameters(effective_count: effective_count)
+    end
+    pm.to_pwm
+  rescue => e
+    raise "PWM creation failed (#{e})"
+  end
+
+  def self.get_pcm(data_model, matrix, effective_count)
+    pm = Bioinform.const_get(data_model).new(matrix)
+    if effective_count && [:PPM].include?(data_model.to_sym)
+      pm.set_parameters(effective_count: effective_count)
+    end
+    pm.to_pcm
+  end
+
+  def self.get_ppm(data_model, matrix)
+    Bioinform.const_get(data_model).new(matrix).to_ppm
+  end
 end
