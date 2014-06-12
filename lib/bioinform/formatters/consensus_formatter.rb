2d089ddc89f1ca502820c0c89fefae6ff5c8a76c
Ilya Vorontsov <prijutme4ty@gmail.com>
2014-06-12 11:39:16 +0400

    consensus formatter

---
+++ lib/bioinform/formatters/consensus_formatter.rb
@@ -0,0 +1,33 @@
+require_relative '../iupac'
+module Bioinform
+  class ConsensusFormatter
+
+    # ConsensusFormatter.new{|pos, el, nucleotide_index| el == pos.max }
+    def initialize(&block)
+      raise Error, 'block is necessary to create an instance of ConsensusFormatter'  unless block_given?
+      @block = block
+    end
+
+    # Simplest consensus formatter which takes into account only maximal elements
+    def self.by_maximal_elements
+      self.new{|pos, el, nucleotide_index| el == pos.max }
+    end
+
+
+    def format_string(pm)
+      pm.each_position.map{|pos| iupac_letter_by_position(pos) }.join
+    end
+
+    def nucleotide_indices_by_position(pos)
+      pos.each_index.select{|nucleotide_index|
+        @block.call(pos, pos[nucleotide_index], nucleotide_index)
+      }
+    end
+
+    def iupac_letter_by_position(pos)
+      nucleotide_indices = nucleotide_indices_by_position(pos)
+      Bioinform::IUPAC::IUPACLettersByNucleotideIndices[nucleotide_indices]
+    end
+    private :nucleotide_indices_by_position, :iupac_letter_by_position
+  end
+end
---
+++ spec/data_models2/consensus_formatter_spec.rb
@@ -0,0 +1,26 @@
+require 'bioinform/data_models_2/pm'
+require 'bioinform/formatters/consensus_formatter'
+
+describe Bioinform::ConsensusFormatter do
+  let(:pm) { Bioinform::MotifModel::PM.new([[10,30,10,28], [30,16,16,16], [12,30,10,26], [26,27,27,1]]) }
+
+  specify('.new without a block raises error') { expect{ Bioinform::ConsensusFormatter.new }.to raise_error Bioinform::Error }
+
+  context 'custom formatter' do
+    let(:formatter){ Bioinform::ConsensusFormatter.new{|pos, el, ind| (pos.max - el) < pos.max * 0.1 } }
+    specify{ expect(formatter.format_string(pm)).to eq 'YACV' }
+  end
+
+  context 'standard formatter' do
+    let(:formatter){ Bioinform::ConsensusFormatter.by_maximal_elements }
+    specify{ expect(formatter.format_string(pm)).to eq 'CACS' }
+  end
+
+  specify do
+    expect{|b|
+      Bioinform::ConsensusFormatter.new(&b).format_string(pm)
+      }.to yield_successive_args( *([ [[10,30,10,28],10,0],     # col,el,ind
+                                      [[10,30,10,28],30,1] ] +  # col,el,ind
+                                      [Array]*14 ) )            # rest triples
+  end
+end
