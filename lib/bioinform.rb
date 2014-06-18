894131bcbf63818c625c3a7a794f64789a863795
Ilya Vorontsov <prijutme4ty@gmail.com>
2014-06-18 11:23:08 +0400

    load conversion algorithms

--- lib/bioinform.rb
+++ lib/bioinform.rb
@@ -1,8 +1,9 @@
 require_relative 'bioinform/version'
 require_relative 'bioinform/support'
 require_relative 'bioinform/parsers'
-require_relative 'bioinform/formatters'
 require_relative 'bioinform/data_models'
+require_relative 'bioinform/conversion_algorithms'
+require_relative 'bioinform/formatters'
 require_relative 'bioinform/cli'
 
 require_relative 'bioinform/background'
---
+++ lib/bioinform/conversion_algorithms.rb
@@ -0,0 +1,3 @@
+require_relative 'conversion_algorithms/pcm2ppm_converter'
+require_relative 'conversion_algorithms/pcm2pwm_converter'
+require_relative 'conversion_algorithms/ppm2pcm_converter'
