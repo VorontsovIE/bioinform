93844e2f0dd3fceefa84ae350a4f3cba089e9f28
Ilya Vorontsov <prijutme4ty@gmail.com>
2014-06-18 13:17:38 +0400

    fix bioinform.get_pcm .get_ppm .get_pwm

--- lib/bioinform.rb
+++ lib/bioinform.rb
@@ -18,33 +18,65 @@ module Bioinform
   class Error < StandardError
   end
 
+  def self.get_model(data_model, matrix, name)
+    Bioinform::MotifModel.const_get(data_model).new(matrix).named(name)
+  end
+
+  def self.get_model_from_string(data_model, matrix_string)
+    motif_infos = Parser.choose(matrix_string).parse(matrix_string)
+    get_model(data_model, motif_infos.matrix, name)
+  end
+
   def self.get_pwm(data_model, matrix, background, pseudocount, effective_count)
-    pm = Bioinform.const_get(data_model).new(matrix)
-    pm.set_parameters(background: background)
-    if pseudocount && ! pseudocount.blank? && [:PCM,:PPM].include?(data_model.to_sym)
-      pm.set_parameters(pseudocount: pseudocount)
+    input_model = get_model_from_string(data_model, matrix)
+    case input_model
+    when MotifModel::PPM
+      ppm2pcm_converter = ConversionAlgorithms::PPM2PCM.new(count: effective_count)
+      pcm2pwm_converter = ConversionAlgorithms::PCM2PWM.new(background: background, pseudocount: pseudocount)
+      pcm2pwm_converter.convert(ppm2pcm_converter.convert(input_model))
+    when MotifModel::PCM
+      pcm2pwm_converter = ConversionAlgorithms::PCM2PWM.new(background: background, pseudocount: pseudocount)
+      pcm2pwm_converter.convert(input_model)
+    when MotifModel::PWM
+      input_model
+    else
+      raise Error, "Unknown input `#{input_model}`"
     end
-    if effective_count && [:PPM].include?(data_model.to_sym)
-      pm.set_parameters(effective_count: effective_count)
-    end
-    pm.to_pwm
   rescue => e
     raise Error, "PWM creation failed (#{e})"
   end
 
   def self.get_pcm(data_model, matrix, effective_count)
-    pm = Bioinform.const_get(data_model).new(matrix)
-    if effective_count && [:PPM].include?(data_model.to_sym)
-      pm.set_parameters(effective_count: effective_count)
+    input_model = get_model_from_string(data_model, matrix)
+    case input_model
+    when MotifModel::PPM
+      ppm2pcm_converter = ConversionAlgorithms::PPM2PCM.new(count: effective_count)
+      ppm2pcm_converter.convert(input_model)
+    when MotifModel::PCM
+      input_model
+    when MotifModel::PWM
+      raise Error, 'Conversion PWM-->PCM not yet implemented'
+    else
+      raise Error, "Unknown input `#{input_model}`"
     end
-    pm.to_pcm
   rescue => e
-    raise Error, "PWM creation failed (#{e})"
+    raise Error, "PCM creation failed (#{e})"
   end
 
   def self.get_ppm(data_model, matrix)
-    Bioinform.const_get(data_model).new(matrix).to_ppm
+    input_model = get_model_from_string(data_model, matrix)
+    case input_model
+    when MotifModel::PPM
+      input_model
+    when MotifModel::PCM
+      pcm2ppm_converter = ConversionAlgorithms::PCM2PPM.new
+      pcm2ppm_converter.convert(input_model)
+    when MotifModel::PWM
+      raise Error, 'Conversion PWM-->PPM not yet implemented'
+    else
+      raise Error, "Unknown input `#{input_model}`"
+    end
   rescue => e
-    raise Error, "PWM creation failed (#{e})"
+    raise Error, "PPM creation failed (#{e})"
   end
 end
