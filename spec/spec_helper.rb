bd1850dd748fcc68f393795c2505791e90ba69c0
Ilya Vorontsov <prijutme4ty@gmail.com>
2014-06-20 12:31:39 +0400

    remove fabrication

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,8 +6,8 @@ require 'rspec-given'
 
 require 'fileutils'
 require 'stringio'
-require 'fabrication'
+# require 'fabrication'
 
 require 'fakefs/spec_helpers'
 require 'spec_helper_source'
-Fabrication.clear_definitions
+# Fabrication.clear_definitions
