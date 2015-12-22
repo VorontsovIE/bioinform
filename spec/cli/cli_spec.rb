ef5ca90b0506dace3f76654dd4e696378713487b
VorontsovIE <vorontsov.i.e@gmail.com>
2015-12-22 17:30:01 +0300

    fix specs for slightly different matrices

--- spec/cli/cli_spec.rb
+++ spec/cli/cli_spec.rb
@@ -1,6 +1,25 @@
 require_relative '../spec_helper'
 require 'bioinform/cli'
 
+def compare_positions(pos_1, pos_2, eps: 1e-6)
+  pos_1.zip(pos_2).all?{|el_1, el_2|
+    (el_1 - el_2).abs <= eps
+  }
+end
+
+def compare_matrices(matrix_1, matrix_2, eps: 1e-6)
+  matrix_1.length == matrix_2.length && \
+  matrix_1.zip(matrix_2).all?{|pos_1, pos_2|
+    compare_positions(pos_1, pos_2, eps: eps)
+  }
+end
+
+def compare_models_in_files(file_1, file_2, klass: Bioinform::MotifModel::PM, eps: 1e-6)
+  pm_1 = klass.from_file(file_1)
+  pm_2 = klass.from_file(file_2)
+  pm_1.name == pm_2.name && compare_matrices(pm_1.matrix, pm_2.matrix)
+end
+
 describe Bioinform::CLI do
   describe '.change_folder_and_extension' do
     it 'should change extension and folder' do
--- spec/cli/pcm2pwm_spec.rb
+++ spec/cli/pcm2pwm_spec.rb
@@ -24,56 +24,56 @@ describe Bioinform::CLI::PCM2PWM do
   it 'should transform single PCM to PWM' do
     run_pcm2pwm('KLF4_f2.pcm')
     expect(File.exist?('KLF4_f2.pwm')).to be_truthy
-    expect(File.read('KLF4_f2.pwm')).to eq File.read('KLF4_f2.pwm.result')
+    expect(compare_models_in_files('KLF4_f2.pwm', 'KLF4_f2.pwm.result')).to be_truthy
   end
 
   it 'should transform multiple PCMs to PWMs' do
     run_pcm2pwm('KLF4_f2.pcm SP1_f1.pcm')
 
     expect(File.exist?('KLF4_f2.pwm')).to be_truthy
-    expect(File.read('KLF4_f2.pwm')).to eq File.read('KLF4_f2.pwm.result')
+    expect(compare_models_in_files('KLF4_f2.pwm', 'KLF4_f2.pwm.result')).to be_truthy
 
     expect(File.exist?('SP1_f1.pwm')).to be_truthy
-    expect(File.read('SP1_f1.pwm')).to eq File.read('SP1_f1.pwm.result')
+    expect(compare_models_in_files('SP1_f1.pwm', 'SP1_f1.pwm.result')).to be_truthy
   end
 
   it 'should transform extension to specified with --extension option' do
     run_pcm2pwm('KLF4_f2.pcm --extension=pat')
     expect(File.exist?('KLF4_f2.pat')).to be_truthy
-    expect(File.read('KLF4_f2.pat')).to eq File.read('KLF4_f2.pwm.result')
+    expect(compare_models_in_files('KLF4_f2.pat', 'KLF4_f2.pwm.result')).to be_truthy
   end
 
   it 'should save PWMs into folder specified with --folder option when folder exists' do
     Dir.mkdir('pwm_folder')  unless Dir.exist?('pwm_folder')
     run_pcm2pwm('KLF4_f2.pcm --folder=pwm_folder')
     expect(File.exist?('pwm_folder/KLF4_f2.pwm')).to be_truthy
-    expect(File.read('pwm_folder/KLF4_f2.pwm')).to eq File.read('KLF4_f2.pwm.result')
+    expect(compare_models_in_files('pwm_folder/KLF4_f2.pwm', 'KLF4_f2.pwm.result')).to be_truthy
   end
   it 'should save PWMs into folder specified with --folder option' do
     FileUtils.rm_rf('pwm_folder')  if Dir.exist?('pwm_folder')
     run_pcm2pwm('KLF4_f2.pcm --folder=pwm_folder')
     expect(File.exist?('pwm_folder/KLF4_f2.pwm')).to be_truthy
-    expect(File.read('pwm_folder/KLF4_f2.pwm')).to eq File.read('KLF4_f2.pwm.result')
+    expect(compare_models_in_files('pwm_folder/KLF4_f2.pwm', 'KLF4_f2.pwm.result')).to be_truthy
   end
 
   it 'should process PCMs with names obtained from STDIN' do
     provide_stdin('KLF4_f2.pcm SP1_f1.pcm') { run_pcm2pwm('') }
     expect(File.exist?('KLF4_f2.pwm')).to be_truthy
-    expect(File.read('KLF4_f2.pwm')).to eq File.read('KLF4_f2.pwm.result')
+    expect(compare_models_in_files('KLF4_f2.pwm', 'KLF4_f2.pwm.result')).to be_truthy
 
     expect(File.exist?('SP1_f1.pwm')).to be_truthy
-    expect(File.read('SP1_f1.pwm')).to eq File.read('SP1_f1.pwm.result')
+    expect(compare_models_in_files('SP1_f1.pwm', 'SP1_f1.pwm.result')).to be_truthy
   end
 
   it 'should process PCMs with names obtained from STDIN when there are some options' do
     provide_stdin('KLF4_f2.pcm') { run_pcm2pwm('-e pat') }
     expect(File.exist?('KLF4_f2.pat')).to be_truthy
-    expect(File.read('KLF4_f2.pat')).to eq File.read('KLF4_f2.pwm.result')
+    expect(compare_models_in_files('KLF4_f2.pat', 'KLF4_f2.pwm.result')).to be_truthy
   end
 
   it 'should process PCMs having filename with spaces' do
     run_pcm2pwm('"KLF4 f2 spaced name.pcm"')
     expect(File.exist?('KLF4 f2 spaced name.pwm')).to be_truthy
-    expect(File.read('KLF4 f2 spaced name.pwm')).to eq File.read('KLF4_f2.pwm.result')
+    expect(compare_models_in_files('KLF4 f2 spaced name.pwm', 'KLF4_f2.pwm.result')).to be_truthy
   end
 end
